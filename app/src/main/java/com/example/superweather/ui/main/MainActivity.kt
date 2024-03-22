d5ec2afa6b1ee158b22939fa79c2dd6164b25507
sohan-technonext <sohanasif@gmail.com>
2024-03-22 14:06:38 +0600

    archi: created basic folders

--- app/src/main/java/com/example/superweather/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/superweather/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.superweather
+package com.example.superweather.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.superweather.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
